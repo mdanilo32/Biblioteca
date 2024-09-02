8ab8dbe4b946b0269beef08c3a6dc5079364dceb
mdanilo32 <mdculajaym@alumno.uspg.edu.gt>
2024-09-01 22:23:47 -0600

    Biblioteca

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Biblioteca.Clases;
+
+namespace Biblioteca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a la biblioteca virtual!");
+
+            // Creación de libros individuales
+            Libro libro1 = new Libro("Un mundo feliz", "Aldous Huxley");
+            Libro libro2 = new Libro("Orgullo y prejuicio", "Jane Austen");
+
+            // Mostrar información de un libro
+            Console.WriteLine(libro1.ObtenerInformacion());
+
+            // Crear la instancia de la biblioteca
+            MiBiblioteca miBiblioteca = new MiBiblioteca();
+
+            // Agregar libros a la biblioteca
+            miBiblioteca.AgregarLibro(libro1);
+            miBiblioteca.AgregarLibro(libro2);
+
+            // Mostrar todos los libros en la biblioteca
+            miBiblioteca.MostrarLibros();
+
+            // Eliminar un libro específico
+            miBiblioteca.RemoverLibroPorTitulo("Un mundo feliz");
+
+            // Buscar libros por autor
+            miBiblioteca.EncontrarLibrosPorAutor("Jane Austen");
+        }
+    }
+}
